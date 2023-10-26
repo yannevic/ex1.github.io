53e53d113dcbe0ddcc453d67d3a342feca1d10c7
Nana <68790631+yannevic@users.noreply.github.com>
2023-10-26 18:30:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function leleco(){
+    var valor = document.querySelector(".input").value
+    var resposta = document.querySelector(".resposta");
+    resposta.innerHTML = `Você digitou um ${valor}.`
+}
